b7ecd53b8ef543ad81069ec5af20110285958814
snramirez <54188400+snramirez@users.noreply.github.com>
2022-06-13 16:01:43 -0300

    estadisticas
    Se agregaron algunas estadisticas

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -26,14 +26,14 @@ ctrl.statusDate = async (req, res) => {
 };
 
 ctrl.biddingType = async (req, res) => {
-    let startDate = req.query.startDate;
-    let finishDate = req.query.finishDate;
+    let startDate = new Date(req.query.startDate);
+    let finishDate = new Date(req.query.finishDate);
 
     try{
-        // let biddings = await Bidding.find({firstPG: {$gte: startDate, $lte: finishDate}})
+        // let biddings = await Bidding.find({ContractDate: {$gte: startDate, $lte: finishDate}})
         let biddings = await Bidding.aggregate([
             {$match:{
-                FirstPG:{$lte: finishDate, $gte: startDate}
+                ContractDate:{$lte: finishDate, $gte: startDate}
             }},
             {$group:{
                 _id: '$BiddingType', total: {$sum: 1}
@@ -49,7 +49,87 @@ ctrl.biddingType = async (req, res) => {
     catch (err) {
         console.log(err); 
     }
+};
 
+ctrl.statusCount = async (req, res) => {
+    let startDate = new Date(req.query.startDate);
+    let finishDate = new Date(req.query.finishDate);
+
+    try{
+        // let biddings = await Bidding.find({ContractDate: {$gte: startDate, $lte: finishDate}})
+        let biddings = await Bidding.aggregate([
+            {$match:{
+                ContractDate:{$lte: finishDate, $gte: startDate}
+            }},
+            {$group:{
+                _id: '$Status', total: {$sum: 1}
+            }}
+        ]).exec()
+        console.log("🚀 ~ file: statistics.js ~ line 11 ~ ctrl.statusDate= ~ biddings", biddings)
+        if(biddings.length === 0){
+            res.status(200).json({Error: 'No hay licitaciones'})
+            return
+        }
+        res.status(200).json(biddings);
+    }
+    catch (err) {
+        console.log(err); 
+    }
+};
+
+ctrl.contractor = async (req, res) => {
+    let startDate = new Date(req.query.startDate);
+    let finishDate = new Date(req.query.finishDate);
+    let contractor = req.query.contractor;
+
+    try{
+        // let biddings = await Bidding.find({ContractDate: {$gte: startDate, $lte: finishDate}})
+        let biddings = await Bidding.aggregate([
+            {$match:{
+                ContractDate:{$lte: finishDate, $gte: startDate},
+                Contractor:contractor
+            }},
+            {$count:"Bidding"}
+        ]).exec()
+        console.log("🚀 ~ file: statistics.js ~ line 11 ~ ctrl.statusDate= ~ biddings", biddings)
+        if(biddings.length === 0){
+            res.status(200).json({Error: 'No hay licitaciones'})
+            return
+        }
+        res.status(200).json(biddings);
+    }
+    catch (err) {
+        console.log(err); 
+    }
+};
+
+ctrl.budget = async (req, res) => {
+    let botBudget = req.query.botBudget;
+    let topBudget = req.query.topBudget;
+    let query = {}
+    if(req.query.budgetType == 'OfficialBudget'){
+        query = {
+            OfficialBudget: {$gte: botBudget, $lte: topBudget}
+        }
+    }
+    else{
+        query = {
+            AllocatedBudget: {$gte: botBudget, $lte: topBudget}
+        }
+    }
+
+    try{
+        let biddings = await Bidding.find({OfficialBudget: {$gte: botBudget, $lte: topBudget}}).exec()
+        console.log("🚀 ~ file: statistics.js ~ line 11 ~ ctrl.statusDate= ~ biddings", biddings)
+        if(biddings.length === 0){
+            res.status(200).json({Error: 'No hay licitaciones'})
+            return
+        }
+        res.status(200).json(biddings);
+    }
+    catch (err) {
+        console.log(err); 
+    }
 };
 
 ctrl.create = async (req, res) => {
--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -9,6 +9,10 @@ const statistics = require('../controllers/statistics');
 
 router.get('/statusDate', statistics.statusDate);
 router.get('/biddingType', statistics.biddingType);
+router.get('/statusCount', statistics.statusCount);
+router.get('/contractor', statistics.contractor);
+router.get('/budget', statistics.budget);
+
 router.post('/add', statistics.create);
 
 module.exports = router; 
