eff9883b1f87832523e86e79e1542946ba12f252
snramirez <54188400+snramirez@users.noreply.github.com>
2023-01-23 12:55:24 -0300

    rutas y funcion de guardar feriados

--- src/controllers/saveFile.js
+++ src/controllers/saveFile.js
@@ -91,4 +91,48 @@ ctrl.deleteStatus = (req, res) => {
     }
 };
 
+ctrl.getHoliday = (req, res) => {  
+    try {
+        let types = fs.readFileSync(path.join(__dirname,'..','data','holidays.txt'), 'utf8')
+        .toString().split("\r\n");
+        res.status(200).json(types.filter(type => type !== ''))
+    } 
+    catch (error) {
+        console.log(error)
+        res.status(200).json({msj: 'error al leer el archivo'})
+    }
+    
+};
+
+
+ctrl.saveHoliday = (req, res) => {
+
+    try {
+        fs.appendFileSync(path.join(__dirname,'..','data','holidays.txt'),  req.query.data + '\r\n' );
+        res.status(200).json()
+    }
+    catch (error) {
+        console.log(error)
+        res.status(200).json({msj: 'error al leer el archivo'})
+    }
+};
+
+ctrl.deleteHoliday = (req, res) => {
+    try {
+        let types = fs.readFileSync(path.join(__dirname,'..','data','holidays.txt'), 'utf8').toString().split("\r\n")
+        let newTypes = types.filter(type => (type !== req.query.data && type !== ''));
+        let typesFIlter = '';
+        newTypes.forEach(type => {
+            typesFIlter = typesFIlter.concat(type + "\r\n")
+        });
+        fs.writeFileSync(path.join(__dirname,'..','data','holidays.txt'), typesFIlter);
+        res.status(200).json()
+    } 
+    catch (error) {
+        console.log(error)    
+        res.status(200).json({msj: 'error al leer el archivo'})
+
+    }
+};
+
 module.exports = ctrl;
--- src/routes/saveFile.js
+++ src/routes/saveFile.js
@@ -10,8 +10,13 @@ const saveFile = require('../controllers/saveFile');
 router.get('/type', saveFile.getType);
 router.post('/type/save', saveFile.saveType);
 router.post('/type/delete', saveFile.deleteType);
+
 router.get('/status', saveFile.getStatus);
 router.post('/status/save', saveFile.saveStatus);
 router.post('/status/delete', saveFile.deleteStatus);
+
+router.get('/holiday', saveFile.getHoliday);
+router.post('/holiday/save', saveFile.saveHoliday);
+router.post('/holiday/delete', saveFile.deleteHoliday);
 
 module.exports = router;
