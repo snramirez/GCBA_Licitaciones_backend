27f2ee4f7dce23117ba44d7c69bfe63497fb5a64
snramirez <54188400+snramirez@users.noreply.github.com>
2023-10-05 16:58:45 -0300

    arreglo de stadisticas de contratistas
    no se termino pero ya esta encaminada

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -67,7 +67,10 @@ ctrl.statusCount = async (req, res) => {
         // let biddings = await Bidding.find({ContractDate: {$gte: startDate, $lte: finishDate}})
         let biddings = await Bidding.aggregate([
             {$match:{
-                CallDate:{$lte: finishDate, $gte: startDate},
+                $or: [
+                    {CallDate:{$lte: finishDate, $gte: startDate}},
+                    {ContractDate:{$lte: finishDate, $gte: startDate}},
+                ],
                 Active:true
             }},
             {$group:{
@@ -91,17 +94,36 @@ ctrl.statusCount = async (req, res) => {
 ctrl.contractor = async (req, res) => {
     let startDate = new Date(req.query.startDate);
     let finishDate = new Date(req.query.finishDate);
-    let contractor = req.query.contractor;
-
+    let contractor = req.query.contractor
+  
     try{
-        // let biddings = await Bidding.find({ContractDate: {$gte: startDate, $lte: finishDate}})
         let biddings = await Bidding.aggregate([
+            {$unwind: "$BidQuantity"},
             {$match:{
-                ContractDate:{$lte: finishDate, $gte: startDate},
-                Contractor:contractor
+                $expr:{
+                    $in:[{$toObjectId:contractor},"$BidQuantity.Contractor"],
+                }
             }},
-            {$count:"Bidding"}
+            {$group:{
+                _id: "$BidQuantity._id",
+                res: {$push: "$$ROOT"}
+            }}
         ]).exec()
+
+        // let biddings = await Bidding.aggregate().unwind("BidQuantity")
+        // .redact(
+        //     {$in: [contractorArray, "$BidQuantity.Contractor"]},
+        //     "$$KEEP",
+        //     "$$PRUNE"
+        // )
+        // {$redact:{
+        //     $cond:{
+        //         if: { $in:[contractor, "$BidQuantity.Contractor"]},
+        //         then: "$$KEEP",
+        //         else: "$$PRUNE"
+        //     },
+
+
         console.log("🚀 ~ file: statistics.js ~ line 11 ~ ctrl.statusDate= ~ biddings", biddings)
         if(biddings.length === 0){
             res.status(200).json({Error: 'No hay licitaciones'})
